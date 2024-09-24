f205aecf328d8f94e1c4dc2e6d5c90b9c9c3f2ce
cguzik <cguzik@OMSB101021-P.stevenson.edu>
2024-09-24 10:13:58 -0400

    Overloading Methods

--- ConnectToGit/src/OverloadingMethods.java
+++ ConnectToGit/src/OverloadingMethods.java
@@ -5,7 +5,10 @@ public class OverloadingMethods {
 		// TODO Auto-generated method stub
 		int num = doubleNumber(4);
 		System.out.println(num);
-		double numD = doubleNumber(4.5);
+		double numD ;
+		numD = doubleNumber(4.5);
+		System.out.println(numD);
+		//numD = doubleNumber("one");
 		System.out.println(numD);
 
 	}
@@ -20,4 +23,14 @@ public class OverloadingMethods {
 		double total = n*n;
 		return total;
 	}
+	/*public static double doubleNumber(double n, double m)
+	{
+		
+	}
+	public static String doubleNumber(String n)
+	{ 
+		System.out.println("Can't calculate a string");
+		return;
+	}
+	*/
 }
